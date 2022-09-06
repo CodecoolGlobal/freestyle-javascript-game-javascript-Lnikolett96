9dac5537f63d0ad64adb5491dee06bebcccd1ccf
Lnikolett96 <privat.princess.chaos@gmail.com>
2022-09-06 14:15:04 +0200

    Merge ...

--- game.js
+++ game.js
@@ -1,19 +1,5 @@
 let catPosition = 1;
-
 function init() {
-
-const position = [0, 80, 160, 240, 320, 400, 480, 560, 640];
-const candies = ['asset/cake.png', 'asset/cookie.png', 'asset/cotton-candy.png', 'asset/ice-cream.png', 'asset/lollipop.png'];
-const classForCandies = ['candies', 'candies2', 'candies3', 'candies4', 'candies5', 'candies6', 'candies7', 'candies8', 'candies9'];
-let theParent = document.querySelector('#cmove');
-
-function makeCandies() {
-    let randomInt = parseInt(Math.floor(Math.random() * 10));
-    let randomCandy = parseInt(Math.floor(Math.random() * 5));
-
-    let candiesDiv = document.createElement('div');
-    candiesDiv.classList.add(classForCandies[randomInt]);
-
     window.addEventListener("keydown", (event) => {
         if (event.defaultPrevented) {
             return; // Do nothing if the event was already processed
@@ -37,6 +23,17 @@ function makeCandies() {
         }
     })
 
+}
+
+function makeCandies() {
+    const candies = ['asset/cake.png', 'asset/cookie.png', 'asset/cotton-candy.png', 'asset/ice-cream.png', 'asset/lollipop.png'];
+    const classForCandies = ['candies', 'candies2', 'candies3', 'candies4', 'candies5', 'candies6', 'candies7', 'candies8', 'candies9'];
+    let theParent = document.querySelector('#cmove');
+    let randomInt = parseInt(Math.floor(Math.random() * 10));
+    let randomCandy = parseInt(Math.floor(Math.random() * 5));
+
+    let candiesDiv = document.createElement('div');
+    candiesDiv.classList.add(classForCandies[randomInt]);
     let candiesElement = document.createElement('img');
     candiesElement.src = candies[randomCandy];
     candiesElement.style.height = '70px';
@@ -48,7 +45,5 @@ function makeCandies() {
         theParent.removeChild(candiesDiv);
     }, 5500)
 }
-setInterval(makeCandies,2000);
-
-const position = [-60, 20, 100, 180, 260, 340, 420, 500]
 init()
+setInterval(makeCandies,2000);
