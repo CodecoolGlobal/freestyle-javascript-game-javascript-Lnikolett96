77f4494c1d6a9c6e8718518afb52c6da0e1d9144
Drew85 <40467936+Drew85@users.noreply.github.com>
2022-09-06 14:10:39 +0200

    Resolve merge conflicts :white_check_mark:

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let catPosition = 1;
+
+function init() {
 
 const position = [0, 80, 160, 240, 320, 400, 480, 560, 640];
 const candies = ['asset/cake.png', 'asset/cookie.png', 'asset/cotton-candy.png', 'asset/ice-cream.png', 'asset/lollipop.png'];
@@ -10,6 +13,29 @@ function makeCandies() {
 
     let candiesDiv = document.createElement('div');
     candiesDiv.classList.add(classForCandies[randomInt]);
+
+    window.addEventListener("keydown", (event) => {
+        if (event.defaultPrevented) {
+            return; // Do nothing if the event was already processed
+        }
+        console.log(event.key)
+        if (event.key === "ArrowDown") {
+            let moveCat = document.querySelector("#moveNyan");
+            moveCat.style.animation = "Down" + catPosition;
+            moveCat.style.animationFillMode = "forwards";
+            moveCat.style.animationDuration = "0.5s";
+            catPosition++;
+            console.log(catPosition)
+        }
+        if (event.key === "ArrowUp") {
+            let moveCat = document.querySelector("#moveNyan");
+            moveCat.style.animation = "Up" + catPosition;
+            moveCat.style.animationFillMode = "forwards";
+            moveCat.style.animationDuration = "0.5s";
+            catPosition--;
+            console.log(catPosition)
+        }
+    })
 
     let candiesElement = document.createElement('img');
     candiesElement.src = candies[randomCandy];
@@ -24,10 +50,5 @@ function makeCandies() {
 }
 setInterval(makeCandies,2000);
 
-
-
-
-
-
-
-
+const position = [-60, 20, 100, 180, 260, 340, 420, 500]
+init()
