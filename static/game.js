2a93761fa33d1eeaa7168dfafd7a90d6fa73fb73
Lnikolett96 <privat.princess.chaos@gmail.com>
2022-09-08 10:15:53 +0200

    Added colision logic for the game

--- static/game.js
+++ static/game.js
@@ -1,5 +1,5 @@
-let catPosition = 4;
 const classForPosition = ['pos1', 'pos2', 'pos3', 'pos4', 'pos5', 'pos6', 'pos7', 'pos8', 'pos9'];
+let catPosition = 4;
 let point = 0;
 
 let startButton = document.getElementById('start')
@@ -47,15 +47,16 @@ function init() {
     setInterval(makeAssumable, 500, baddies);setInterval(makeAssumable,2000, candies);setInterval(takeCandy, 1000)
 }
 
-function makeAssumable(searchList) {
-    const classForCandies = ['candies', 'candies2', 'candies3', 'candies4', 'candies5', 'candies6', 'candies7', 'candies8', 'candies9'];
+function makeCandies() {
+    const candies = ['/static/asset/cake.png', '/static/asset/cookie.png', '/static/asset/cotton-candy.png', '/static/asset/ice-cream.png', '/static/asset/lollipop.png'];
 
     let theParent = document.querySelector('#cmove');
     let randomInt = parseInt(Math.floor(Math.random() * 9));
     let randomCandy = parseInt(Math.floor(Math.random() * 5));
 
     let candiesDiv = document.createElement('div');
-    candiesDiv.classList.add(classForCandies[randomInt]);
+    candiesDiv.classList.add('candy');
+    candiesDiv.classList.add(classForPosition[randomInt]);
     let candiesElement = document.createElement('img');
     candiesElement.src = candies[randomCandy];
     candiesElement.style.height = '70px';
@@ -76,12 +77,19 @@ function takeCandy() {
     let rectnyan = nyancat.getBoundingClientRect();
     for (let i = 0; i < activCandies.length; i++) {
         let oneCandyRect = activCandies[i].getBoundingClientRect();
-        console.log('rectnyan: ' + rectnyan.top + ' onecandyleft: ' + oneCandyRect.top)
-        if (rectnyan.top === oneCandyRect.top && Math.floor(oneCandyRect.right) >= 1200) {
+        if (rectnyan.x < oneCandyRect.x + oneCandyRect.width &&
+            rectnyan.x + rectnyan.width > oneCandyRect.x &&
+            rectnyan.y < oneCandyRect.y + oneCandyRect.height &&
+            rectnyan.y + rectnyan.height > oneCandyRect.y) {
             console.log('belement')
             activCandies[i].remove()
             point += 20;
+
         }
+
+
     }
 }
-init()
+
+
+//if (rectnyan.top === oneCandyRect.top && oneCandyRect.right === 1038)
